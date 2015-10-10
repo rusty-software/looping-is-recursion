b751995924e7012b21447afeb14f3a2752ba8278
Rusty Bentley <mycoding.alert@gmail.com>
2015-10-10 18:44:11 -0500

    seq= passing

--- src/looping_is_recursion.clj
+++ src/looping_is_recursion.clj
@@ -15,7 +15,14 @@
     (f (first a-seq) a-seq)))
 
 (defn seq= [seq1 seq2]
-  ":(")
+  (let [f (fn [coll1 coll2]
+            (cond
+              (and (empty? coll1) (empty? coll2)) true
+              (and (empty? coll1) (seq coll2)) false
+              (and (empty? coll2) (seq coll1)) false
+              (not= (first coll1) (first coll2)) false
+              :else (recur (rest coll1) (rest coll2))))]
+    (f seq1 seq2)))
 
 (defn find-first-index [pred a-seq]
   ":(")
